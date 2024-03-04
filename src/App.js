aa5db57c06944f4685f22a4eb56cbcae8ef7c1f1
shahin <shahinahd3@gamil.com>
2024-03-04 16:00:25 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import PokemonList from "./components/PokemonList";
+import Pagination from "./components/Pagination"
 import axios from "axios";
 
 function App() {
@@ -12,20 +13,39 @@ function App() {
 
   useEffect(() => {
     setLoading(true)
-    axios.get(currentPageUrl).then((res) => {
+    let cancel
+    axios.get(currentPageUrl,{
+      cancelToken: new axios.CancelToken(c=> cancel = c)
+    }).then((res) => {
       setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map((p) => p.name));
     });
+
+    return ()=>{
+      cancel()
+    }
   }, [currentPageUrl]);
+
+  function gotoNextPage() {
+    setcurrentPageUrl(nextPageUrl)
+  }
+
+  function gotoPrevPage(){
+    setcurrentPageUrl(prevPagePageUrl)
+  }
+
 
   if( loading) return "Loading..."
 
   return (
-    <div>
+    <>
       <PokemonList pokemon={pokemon} />
-    </div>
+      <Pagination gotoNextPage={ 
+        nextPageUrl ? gotoNextPage: null }
+      gotoPrevPage={prevPagePageUrl ? gotoPrevPage : null} />
+    </>
   );
 }
 
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pagination = ({gotoNextPage,gotoPrevPage}) => {
+  return (
+    <div>
+        {gotoPrevPage && <button onClick={gotoPrevPage} >Previous</button>}
+        {gotoNextPage && <button onClick={gotoNextPage} >Next</button>}
+    </div>
+  )
+}
+
+export default Pagination
